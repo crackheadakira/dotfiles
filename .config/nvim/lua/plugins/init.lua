56736f0aadce5966503d4611259997a234a0d00c
crackheadakira <lasn.mine@gmail.com>
2024-03-03 23:40:57 +0100

    Add smooth scrolling

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -26,4 +26,10 @@ return {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
     },
+    {
+        "karb94/neoscroll.nvim",
+        config = function()
+            require('neoscroll').setup {}
+        end
+    }
 }
